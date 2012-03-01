55ae3b35e7648ace7347dc7381c00199447222b6
Corin Fletcher <corin@westcirque.co.uk>
2012-03-01 21:42:25 +0000

    another change

--- src/main/java/uk/co/westcirque/test/Test.java
+++ src/main/java/uk/co/westcirque/test/Test.java
@@ -2,6 +2,6 @@ package uk.co.westcirque.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World...");
     }
 }
